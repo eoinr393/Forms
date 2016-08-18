29259c23ae40c0752f72cdccf603a8949fc065d8
Bryan Duggan <skooter500@gmail.com>
2016-08-18 09:18:40 +0100

    bla

---
+++ Assets/Code/SnakeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using BGE;
+
+public class SnakeGenerator : MonoBehaviour {
+
+    public int numParts = 20;
+    public float gap;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < numParts; i++)
+        {
+            GameObject part = (GameObject) GameObject.Instantiate<GameObject>(prefab);
+            part.transform.parent = transform;
+            part.transform.position = transform.position + new Vector3(0, 0, i * gap);            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
