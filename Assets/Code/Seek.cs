de8ce904339fa90c03c7aacddf3240d7ab876698
skooter500 <skooter500@gmail.com>
2016-08-22 00:18:10 +0100

    bla

---
+++ Assets/Code/Seek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class Seek : SteeringBehaviour
+{
+    public Vector3 seekTargetPos = Vector3.zero;
+    public bool seekPlayer = false;
+    
+    public override Vector3 Calculate()
+    {
+        return boid.Seek(seekTargetPos);    
+    }
+}
---
+++ Assets/Code/SteeringBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(Boid))]
+public abstract class SteeringBehaviour: MonoBehaviour
+{
+    [Range(0.0f, 200.0f)]
+    public float weight = 1.0f;
+
+    public abstract Vector3 Calculate();
+
+    [HideInInspector]
+    public Boid boid;
+
+    public void Start()
+    {
+        boid = GetComponent<Boid>();
+    }
+}
