462271389e04d476ead14d7fa84a4611b3de0cbf
Bryan Duggan <skooter500@gmail.com>
2016-10-05 21:27:43 +0100

    bla

---
+++ Assets/Code/Formation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Formation : SteeringBehaviour
+{
+    public Boid leader;
+    private Vector3 offset;
+    private Vector3 targetPos;
+
+    public void Start()
+    {
+        if (leader != null)
+        {
+            offset = transform.position - leader.transform.position;
+            offset = Quaternion.Inverse(transform.rotation) * offset;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (isActiveAndEnabled)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position, targetPos);
+        }
+    }
+
+    public override Vector3 Calculate()
+    {
+        Vector3 target = Vector3.zero;
+
+        target = leader.TransformPoint(offset);
+        
+        float dist = (target - boid.position).magnitude;
+
+        float lookAhead = (dist / boid.maxSpeed);
+
+        target = target + (lookAhead * leader.velocity);
+
+        /*float pitchForce = target.y - position.y;
+        pitchForce *= (1.0f - pitchForceScale);
+        target.y -= pitchForce;
+
+        offsetPursuitTargetPos = target;
+
+        Utilities.checkNaN(target);
+        */
+        return boid.ArriveForce(target);
+    }
+}
