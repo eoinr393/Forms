cbae3e8410f6d86ddd73a1890b562ee0da51755e
skooter500 <skooter500@gmail.com>
2016-12-07 23:04:53 +0000

    new model test

--- Assets/ResetPosition.cs
+++ Assets/ResetPosition.cs
@@ -5,11 +5,16 @@ public class ResetPosition : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	    
+        orig = transform.position;
 	}
-	
+
+    Vector3 orig;
+    public GameObject plane;
 	// Update is called once per frame
 	void Update () {
-	
+        if (transform.position.y > plane.transform.position.y + 100)
+        {
+            transform.position = orig;
+        }
 	}
 }
---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    float speed = 5.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+}
